729cf9f39aa9a577b856223fdb82b6ca962f52bd
u47326 <u47326@B534DCOK.ustr.com>
2019-07-17 14:13:08 +0530

    testbase change

--- Guru99_Banking_Project/src/main/java/com/gurubanking/testbase/TestBase.java
+++ Guru99_Banking_Project/src/main/java/com/gurubanking/testbase/TestBase.java
@@ -30,7 +30,7 @@ public class TestBase {
 	public static WebDriver driver;
 	
 	static ReadConfigFile readconfig = new ReadConfigFile();
-	public SeleniumElements element = new SeleniumElements();
+	public SeleniumElements elements = new SeleniumElements();
 	
 	
 	@Parameters("browser")
